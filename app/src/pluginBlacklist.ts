8097eb710cd0fb7c32258ed87bfa80a87a90ee15
fxq <3200017240@qq.com>
2026-07-27 13:59:29 +0800

    chore: disable bundled plugin manager

--- app/src/pluginBlacklist.ts
+++ app/src/pluginBlacklist.ts
@@ -1,11 +1,12 @@
 export const PLUGIN_BLACKLIST = [
     'terminus-shell-selector', // superseded by profiles
     'terminus-scrollbar', // now useless
-    'terminus-clickable-links', // now bundled with Tabby
-    'tabby-clickable-links', // now bundled with Tabby
-    'terminus-clickable-ips', // broken, functionality now bundled with Tabby
+    'terminus-clickable-links', // now bundled with Ferrum
+    'tabby-clickable-links', // now bundled with Ferrum
+    'terminus-clickable-ips', // broken, functionality now bundled with Ferrum
     'terminus-elastic-quick-commands', // broken and abandoned, fork of quick-commands
     'terminus-elastic-quick-cmds', // broken and abandoned, fork of quick-commands
     'tabby-fig', // abandoned,
     'tabby-plugin-fig-integration', // abandoned,
+    'tabby-plugin-manager', // disabled to reduce memory; re-enable when needed
 ]
